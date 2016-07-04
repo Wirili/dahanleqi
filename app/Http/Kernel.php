c880eda3cb40e8304405428f257e2080111ae2e1
332477694@qq.com <w19871110>
2016-07-05 00:11:38 +0800

    权限控制

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
         ],
-        'wechat' => [
+        'wechat_web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -55,5 +55,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'guest:admin' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'wechat' => \App\Http\Middleware\WechatAuthenticate::class,
     ];
 }
--- app/Http/Middleware/WechatAuthenticate.php
+++ app/Http/Middleware/WechatAuthenticate.php
@@ -4,6 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Wechat;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Socialite;
 
 /**
  * Class WechatAuthenticate
@@ -13,17 +16,40 @@ class WechatAuthenticate
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (!session('wechat.oauth_user')) {
+        if (!Auth::guard($guard)->check()) {
             if ($request->has('state') && $request->has('code')) {
-                session(['wechat.oauth_user' => Wechat::Oauth()->user()]);
-
+                $wechat_user = Wechat::oauth()->user();
+                $user = Socialite::where('openid',$wechat_user->getId())->first();
+                if (!$user && $wechat_user->getToken()->scope == 'snsapi_base') {
+                    return Wechat::oauth()->scopes(['snsapi_userinfo'])->redirect($request->fullUrl());
+                } elseif ($user) {
+                    Auth::login($user->user);
+                } else {
+                    $original = $wechat_user->getOriginal();
+                    $user = new User();
+                    $user->name = $original['nickname'];
+                    $user->email = 'SJ' . date('YmdHis') . rand(10000, 99999) . '@sj.com';
+                    $user->password = \Hash::make(rand(10000, 99999));
+                    $user->save();
+                    $socialite = new Socialite();
+                    $socialite->user_id = $user->user_id;
+                    $socialite->openid = $original['openid'];
+                    $socialite->nickname = $original['nickname'];
+                    $socialite->sex = $original['sex'];
+                    $socialite->province = $original['province'];
+                    $socialite->city = $original['city'];
+                    $socialite->country = $original['country'];
+                    $socialite->headimgurl = $original['headimgurl'];
+                    $socialite->save();
+                    Auth::login($user);
+                }
                 return redirect()->to($this->getTargetUrl($request));
             }
 
@@ -33,11 +59,32 @@ class WechatAuthenticate
                 $scopes = array_map('trim', explode(',', $scopes));
             }
 
-            return Wechat::Oauth()->scopes($scopes)->redirect($request->fullUrl());
+            return Wechat::oauth()->scopes($scopes)->redirect($request->fullUrl());
         }
 
         return $next($request);
     }
+
+//    public function handle($request, Closure $next, $guard = null)
+//    {
+//        if (!session('wechat.oauth_user')) {
+//            if ($request->has('state') && $request->has('code')) {
+//                session(['wechat.oauth_user' => Wechat::oauth()->user()]);
+//
+//                return redirect()->to($this->getTargetUrl($request));
+//            }
+//
+//            $scopes = config('wechat.oauth.scopes', ['snsapi_base']);
+//
+//            if (is_string($scopes)) {
+//                $scopes = array_map('trim', explode(',', $scopes));
+//            }
+//
+//            return Wechat::oauth()->scopes($scopes)->redirect($request->fullUrl());
+//        }
+//
+//        return $next($request);
+//    }
 
     /**
      * Build the target business url.
@@ -50,6 +97,6 @@ class WechatAuthenticate
     {
         $queries = array_except($request->query(), ['code', 'state']);
 
-        return $request->url().(empty($queries) ? '' : '?'.http_build_query($queries));
+        return $request->url() . (empty($queries) ? '' : '?' . http_build_query($queries));
     }
 }
