b7e3dc84a22f2f775a0d91f32b57f9e0b4a7f19c
Tms <332477694@qq.com>
2016-07-07 15:53:30 +0800

    权限控制

--- database/seeds/SocialitesTableSeeder.php
+++ database/seeds/SocialitesTableSeeder.php
@@ -13,18 +13,18 @@ class SocialitesTableSeeder extends Seeder
     public function run()
     {
         //
-        $list=[[
-            'user_id'=>2,
-            'openid'=>'oVWc5xIPC1BK4Nhj1M4eEGcBVCHg',
-            'nickname'=>'霖',
-            'sex'=>1,
-            'city'=>'汕头',
-            'country'=>'中国',
-            'province'=>'广东',
-            'headimgurl'=>'http://wx.qlogo.cn/mmopen/05iayxiaJMoGI0XHSmRSv28cNqibb9YJe7UIXfH7WvGLO6dHsNhgl655ibRzcVXr6VAVUH2vJNh4YByibyiblQPVHxB7B3VFhu94Wic/0',
-        ]];
-        foreach ($list as $item) {
-            Socialite::create($item);
-        }
+//        $list=[[
+//            'user_id'=>2,
+//            'openid'=>'oVWc5xIPC1BK4Nhj1M4eEGcBVCHg',
+//            'nickname'=>'霖',
+//            'sex'=>1,
+//            'city'=>'汕头',
+//            'country'=>'中国',
+//            'province'=>'广东',
+//            'headimgurl'=>'http://wx.qlogo.cn/mmopen/05iayxiaJMoGI0XHSmRSv28cNqibb9YJe7UIXfH7WvGLO6dHsNhgl655ibRzcVXr6VAVUH2vJNh4YByibyiblQPVHxB7B3VFhu94Wic/0',
+//        ]];
+//        foreach ($list as $item) {
+//            Socialite::create($item);
+//        }
     }
 }
---
+++ database/seeds/WechatMenuTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\WechatMenu;
+
+class WechatMenuTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $list = [[
+            'name' => '优乐柔',
+            'type' => 'view',
+            'url' => 'http://ylr.ch-city.com'
+        ], [
+            'name' => '活动指南',
+            'type' => 'view',
+            'url' => 'http://ylr.ch-city.com'
+        ], [
+            'parent_id' => 2,
+            'name' => '正品查询',
+            'type' => 'view',
+            'url' => 'http://ylr.ch-city.com/index/quality'
+        ], [
+            'parent_id' => 2,
+            'name' => '转盘抽奖',
+            'type' => 'view',
+            'url' => 'http://ylr.ch-city.com/award/index'
+        ]];
+        foreach ($list as $item) {
+            WechatMenu::create($item);
+        }
+    }
+}
