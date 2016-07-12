4d5987ef08c51f5dce5d740912c83dcda3b19139
Wirili <332477694@qq.com>
2016-07-13 01:06:46 +0800

    权限控制

--- resources/views/default/goods.blade.php
+++ resources/views/default/goods.blade.php
@@ -33,7 +33,8 @@
                     $(function(){
                         var swiper = new Swiper('.swiper-container', {
                             pagination: '.swiper-pagination',
-                            paginationClickable: true
+                            paginationClickable: true,
+                            loop: true
                         });
                     });
                 </script>
