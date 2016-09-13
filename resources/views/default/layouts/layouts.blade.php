0ff9acf7a77bafb02cb1c078f2d737b5f6a2d729
Tms <332477694@qq.com>
2016-09-13 16:59:47 +0800

    权限控制

--- resources/views/default/layouts/layouts.blade.php
+++ resources/views/default/layouts/layouts.blade.php
@@ -45,7 +45,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{URL::route('index')}}">优乐柔</a>
+                <a class="navbar-brand" href="{{URL::route('index')}}">大韩乐奇</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="navbar-home">
